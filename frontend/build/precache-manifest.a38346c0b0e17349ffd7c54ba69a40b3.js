94a040af2085f43c8916b0111f3541b3a111c847
Siddharth Mishra <107067738+SiddharthMishra2703@users.noreply.github.com>
2023-06-03 23:39:24 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.a38346c0b0e17349ffd7c54ba69a40b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "220ae661a7623e51ba51",
+    "url": "/static/css/main.6a420fde.chunk.css"
+  },
+  {
+    "revision": "220ae661a7623e51ba51",
+    "url": "/static/js/main.dc08a4b9.chunk.js"
+  },
+  {
+    "revision": "06db185ad530a60a72ed",
+    "url": "/static/js/runtime~main.40c40c78.js"
+  },
+  {
+    "revision": "11e5d40f3eaae282afe4",
+    "url": "/static/css/2.5e8742fa.chunk.css"
+  },
+  {
+    "revision": "11e5d40f3eaae282afe4",
+    "url": "/static/js/2.00c543d6.chunk.js"
+  },
+  {
+    "revision": "36787e65c8dde2cdd8bf",
+    "url": "/static/js/3.7cabcc74.chunk.js"
+  },
+  {
+    "revision": "51593035566a87d8be8c20a87a48d15d",
+    "url": "/index.html"
+  }
+];
